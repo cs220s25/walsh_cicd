89b2479af3f70c9347590d41a4afe4e02e12edb4
Jon Walsh <walshj05@moravian.edu>
2025-04-10 08:28:08 -0400

    added allowing execution permissions to userdata script

--- userdata.sh
+++ userdata.sh
@@ -2,6 +2,7 @@
 yum install git -y
 git clone https://github.com/cs220s25/walsh_cicd /walsh_cicd
 cd /walsh_cicd
+chmod +x redeploy.sh
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
 cp web_counter.service /etc/systemd/system
